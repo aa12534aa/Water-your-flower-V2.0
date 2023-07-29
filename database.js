4dffd7cb12dc0cc9818b0758feea3e0e7cb87b70
aa_12534_aa <86166831+aa12534aa@users.noreply.github.com>
2023-07-29 10:46:16 +0200

    Add files via upload

---
+++ database.js
@@ -0,0 +1,58 @@
+export let flowers = JSON.parse(localStorage.getItem('flowers')) || [
+    {
+        id: 'id1',
+        img: 'zdj/albacja.JPG',
+        time: 1,
+        waterDate: new Date(),
+        name: 'albacja jedwabista',
+        waterTxt: 'When the background color will turn red water your flower!'
+    },    
+    {
+        id: 'id2',
+        img: 'zdj/habanero.JPG',
+        time: 1,
+        waterDate: new Date(),
+        name: 'habanero chocolate',
+        waterTxt: 'When the background color will turn red water your flower!'
+    },
+    {
+        id: 'id3',
+        img: 'zdj/mango.JPG',
+        time: 1,
+        waterDate: new Date(),
+        name: 'mango',
+        waterTxt: 'When the background color will turn red water your flower!'
+    },
+    {
+        id: 'id4',
+        img: 'zdj/oliwka.JPG',
+        time: 1,
+        waterDate: new Date(),
+        name: 'oliwka europejska',
+        waterTxt: 'When the background color will turn red water your flower!'
+    },
+    {
+        id: 'id5',
+        img: 'zdj/palma.JPG',
+        time: 1,
+        waterDate: new Date(),
+        name: 'palma kokosowa',
+        waterTxt: 'When the background color will turn red water your flower!'
+    },
+    {
+        id: 'id6',
+        img: 'zdj/pomidor.JPG',
+        time: 1,
+        waterDate: new Date(),
+        name: 'pomidor',
+        waterTxt: 'When the background color will turn red water your flower!'
+    },
+    {
+        id: 'id7',
+        img: 'zdj/truskawka.JPG',
+        time: 1,
+        waterDate: new Date(),
+        name: 'truskawka',
+        waterTxt: 'When the background color will turn red water your flower!'
+    }
+]
